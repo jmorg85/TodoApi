7159c473bb0aa1cae284c366df841ffb3f877c18
Justice <Justice@DESKTOP-ODOCO9L>
2024-01-21 15:33:08 -0600

    Add project files.

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using TodoApi.Models;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+
+        }
+        public DbSet<TodoApi.Models.TodoItemDTO> TodoItem { get; set; } = default!;
+    }
+}
